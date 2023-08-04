1b5dcf28b3dbfee6eeade0d9c659d753fb793dbf
Giulio Ungaretti <giulioungaretti@me.com>
2023-08-04 21:22:19 +0200

    Add Tests for the Sensor.cs

--- Tests/SensorTest.cs
+++ Tests/SensorTest.cs
@@ -1,125 +1,46 @@
-﻿using System;
-using System.Threading;
+﻿using IndustrialControlService.Components;
 
-using Moq;
+namespace Tests;
 
-using Xunit;
-
-namespace IndustrialControlService.Components.Test
+public class SensorTests : IDisposable
 {
-    public class SensorTests : IDisposable
+    private readonly PressureSensor pressureSensor = new(0, 10, 1000);
+    private readonly TemperatureSensor temperatureSensor = new(-50, 50, 1000);
+    private readonly CancellationTokenSource source = new();
+
+    [Fact]
+    public void SetpointWithinSafetyRange()
     {
-        private readonly PressureSensor pressureSensor;
-        private readonly TemperatureSensor temperatureSensor;
-        private readonly CancellationTokenSource source;
+        double setpointValue = 7.5;
+        pressureSensor.Setpoint = setpointValue;
 
-        public SensorTests()
-        {
-            pressureSensor = new PressureSensor(0, 10, 1000);
-            temperatureSensor = new TemperatureSensor(-50, 50, 1000);
-            source = new CancellationTokenSource();
-        }
-
-        [Fact]
-        public void SetpointWithinSafetyRange()
-        {
-            double setpointValue = 7.5;
-            pressureSensor.Setpoint = setpointValue;
-
-            Assert.Equal(setpointValue, pressureSensor.Setpoint);
-        }
-
-        [Fact]
-        public void SetpointOutOfRangeThrowsException()
-        {
-            double setpointValue = 15;
-
-            Assert.Throws<ArgumentOutOfRangeException>(() => pressureSensor.Setpoint = setpointValue);
-        }
-
-        [Fact]
-        public void UpdateValue()
-        {
-            double initialValue = 0;
-            double setpointValue = 5;
-            double expectedValue = initialValue + (setpointValue - initialValue) * 0.1;
-
-            var mockHandler = new Mock<HttpMessageHandler>();
-            mockHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), source.Token)
-                .ReturnsAsync(new HttpResponseMessage()
-                {
-                    StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent("{\"temperature\":22 }")
-                });
-
-            temperatureSensor.Setpoint = setpointValue;
-
-            Thread.Sleep(10000);
-
-            Assert.Equal(expectedValue, temperatureSensor.Value, 1);
-        }
-
-        public void Dispose()
-        {
-            source?.Cancel();
-        }
-    }
-}
-
-public class SensorTests
-{
-    [Theory]
-    [InlineData(-50, 50, 500)]
-    [InlineData(0, 100, 1000)]
-    public void Setpoint_SetWithinSafetyRange(double safetyMin, double safetyMax, int scanRate)
-    {
-        //Arrange
-        var mockTimer = new Mock<Timer>(null);
-        var sensor = new Mock<Sensor>(safetyMin, safetyMax, scanRate) { CallBase = true };
-        sensor.Setup(s => s.UpdateValue());
-        sensor.Setup(s => s.Dispose());
-
-        //Act
-        double expected = 50;
-        sensor.Object.Setpoint = expected;
-        double actual = sensor.Object.Setpoint;
-
-        //Assert
-        Assert.Equal(expected, actual);
+        Assert.Equal(setpointValue, pressureSensor.Setpoint);
     }
 
-    [Theory]
-    [InlineData(-50, 50, 500, -100)]
-    [InlineData(0, 100, 1000, 150)]
-    public void Setpoint_SetOutsideSafetyRange(double safetyMin, double safetyMax, int scanRate, double value)
+    [Fact]
+    public void SetpointOutOfRangeThrowsException()
     {
-        //Arrange
-        var mockTimer = new Mock<Timer>(null);
-        var sensor = new Mock<Sensor>(safetyMin, safetyMax, scanRate) { CallBase = true };
-        sensor.Setup(s => s.UpdateValue());
-        sensor.Setup(s => s.Dispose());
+        double setpointValue = 15;
 
-        //Act & Assert
-        Assert.Throws<ArgumentOutOfRangeException>(() => sensor.Object.Setpoint = value);
+        Assert.Throws<ArgumentOutOfRangeException>(() => pressureSensor.Setpoint = setpointValue);
     }
 
     [Fact]
     public void UpdateValue()
     {
-        //Arrange
-        var mockTimer = new Mock<Timer>(null);
-        var sensor = new Mock<Sensor>(50, 100, 1000) { CallBase = true };
-        sensor.Setup(s => s.Dispose());
+        double setpointValue = 5;
 
-        //Act
-        double expected = 70;
-        sensor.Object.Setpoint = 70;
-        sensor.Object.UpdateValue();
-        double actual = sensor.Object.GetPrivateField<double>("value");
-
-        //Assert
-        Assert.Equal(expected, actual, 3);
+        temperatureSensor.Setpoint = setpointValue;
+        // test non linear response 
+        Assert.NotEqual(setpointValue, temperatureSensor.Value, 1);
+        // let it settle
+        Thread.Sleep(1000);
+        // must have reached the setpoint
+        Assert.Equal(setpointValue, temperatureSensor.Value, 1);
     }
-}
 
+    public void Dispose()
+    {
+        source?.Cancel();
+    }
+};
